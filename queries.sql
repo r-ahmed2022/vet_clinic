c871b1595cc2180d0595d15bcab1b60bd92b5dd3
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-02-20 20:49:17 +0530

    created database and animals table

--- queries.sql
+++ queries.sql
@@ -1,3 +1,17 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+select * from table where Name like '%mon'
+
+select name from animals where extract(year from date_of_birth) between '2016' and '2019'
+
+select name from animals where neutered = true and escape_attempts < 3
+
+select date_of_birth from animals where name = 'Agumon' or name ='Pikachu'
+
+select name, escape_attempts from animals where weight_kg > 10.5
+
+select * from animals where neutered = true
+
+select * from animals where name not like '%Gabumon'
+
+select * from animals where weight_kg between  10.4 and 17.3
--- schema.sql
+++ schema.sql
@@ -1,5 +1,11 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
+CREATE TABLE animals(
+  id    INT,
+  name VARCHAR(250),
+  date_of_birth   DATE,
+  escape_attempts   INT,
+  neutered boolean,
+  weight_kg decimal,
+  PRIMARY KEY(id) G
 );
